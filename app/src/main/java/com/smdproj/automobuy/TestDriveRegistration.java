b2d828f9e30788951b2f3e88c4fc7a1187ac3c49
Tallhha <talhamustafa3@gmail.com>
2021-10-20 17:26:27 +0500

    TEST

---
+++ app/src/main/java/com/smdproj/automobuy/TestDriveRegistration.java
@@ -0,0 +1,14 @@
+package com.smdproj.automobuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestDriveRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_drive_registration);
+    }
+}
